91b5b383a9f28b19a3ad1f9fb4395b63419b82d1
jerinrajjsss <jerinr960@gmail.com>
2024-08-16 12:56:05 +0530

    update middleware

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -15,20 +15,27 @@ if (!cached) {
     promise: null,
   };
 }
-
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
-  if (!MONGODB_URL) throw new Error("Missing MongoDB connection url");
+  if (!MONGODB_URL) {
+    throw new Error("Missing MongoDB connection URL");
+  }
 
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URL, {
-      dbName: "imaginify",
-      bufferCommands: false,
-    });
+  try {
+    console.log("Connecting to MongoDB...");
+    cached.promise =
+      cached.promise ||
+      mongoose.connect(MONGODB_URL, {
+        dbName: "imaginify",
+        bufferCommands: false,
+      });
 
-  cached.conn = await cached.promise;
-
-  return cached.conn;
+    cached.conn = await cached.promise;
+    console.log("Successfully connected to MongoDB");
+    return cached.conn;
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    throw error;
+  }
 };
--- middelware.ts
+++ middelware.ts
@@ -1,13 +1,11 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-// Make sure that the `/api/webhooks/(.*)` route is not protected here
-export default clerkMiddleware()
+const isProtectedRoute = createRouteMatcher(['/protected(.*)'])
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect()
+})
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)']
 }
